a32ec47a27749b7a598bc2f155f77d4eca256565
FrOxyz06 <elmounirmohamed2006@gmail.com>
2025-05-21 11:23:04 -0700

    Made the toString

--- HTMLManager.java
+++ HTMLManager.java
@@ -18,7 +18,20 @@ public Queue<HTMLTag> getTags() {
     return tags;
 }
 
-
+public String toString() {
+   String code = " ";
+   Queue<HTMLTag> temp = new LinkedList<>();
+   // I put every value in temp
+   while(!tags.isEmpty()){
+     temp.add(tags.remove());
+   }
+   // making a string of all the tags and adding back in tags
+    while(!temp.isEmpty()){
+        code += temp.element() + " ";
+        tags.add(temp.remove());
+    }
+    return code.trim();
+}
 
 
 
